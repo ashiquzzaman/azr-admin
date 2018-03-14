4aa76f77c4b1f2d9723b645b5e20c83c3dca69dd
ashiquzzaman <ashiquzzaman@outlook.com>
2018-03-14 19:28:00 +0600

    change dashboard

--- Development/AzRAdmin/Presentations/AzR.Web/Scripts/app/dashboard.js
+++ Development/AzRAdmin/Presentations/AzR.Web/Scripts/app/dashboard.js
@@ -3,6 +3,7 @@
     Chart.defaults.global.responsive = true;
     ctx = $('#jumbotron-line-chart').get(0).getContext('2d');
     options = {
+        responsive: true,
         showScale: false,
         scaleShowGridLines: false,
         scaleGridLineColor: "rgba(0,0,0,.05)",
@@ -23,19 +24,24 @@
     data = {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
         datasets: [
-          {
-              label: "My Second dataset",
-              fillColor: "rgba(34, 167, 240,0.2)",
-              strokeColor: "#22A7F0",
-              pointColor: "#22A7F0",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "#22A7F0",
-              data: [28, 48, 40, 45, 76, 65, 90]
-          }
+            {
+                label: "My Second dataset",
+                backgroundColor: "rgba(34, 167, 240,0.2)",
+                pointBackgroundColor: "#22A7F0",
+                pointBorderColor: "#22A7F0",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "#22A7F0",
+                data: [28, 48, 40, 45, 76, 65, 90]
+            }
         ]
     };
-    myLineChart = new Chart(ctx).Line(data, options);
+    myLineChart = new Chart(ctx,
+        {
+            type: "line",
+            data: data,
+            options: options
+        });
 });
 
 $(function () {
@@ -43,6 +49,7 @@ $(function () {
     Chart.defaults.global.responsive = true;
     ctx = $('#jumbotron-bar-chart').get(0).getContext('2d');
     option_bars = {
+        responsive: true,
         showScale: false,
         scaleShowGridLines: false,
         scaleBeginAtZero: true,
@@ -60,28 +67,33 @@ $(function () {
     data = {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
         datasets: [
-          {
-              label: "My First dataset",
-              fillColor: "rgba(26, 188, 156,0.6)",
-              strokeColor: "#1ABC9C",
-              pointColor: "#1ABC9C",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "#1ABC9C",
-              data: [65, 59, 80, 81, 56, 55, 40]
-          }, {
-              label: "My Second dataset",
-              fillColor: "rgba(34, 167, 240,0.6)",
-              strokeColor: "#22A7F0",
-              pointColor: "#22A7F0",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "#22A7F0",
-              data: [28, 48, 40, 19, 86, 27, 90]
-          }
+            {
+                label: "My First dataset",
+                fillColor: "rgba(26, 188, 156,0.6)",
+                strokeColor: "#1ABC9C",
+                pointColor: "#1ABC9C",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "#1ABC9C",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }, {
+                label: "My Second dataset",
+                fillColor: "rgba(34, 167, 240,0.6)",
+                strokeColor: "#22A7F0",
+                pointColor: "#22A7F0",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "#22A7F0",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
         ]
     };
-    myBarChart = new Chart(ctx).Bar(data, option_bars);
+    myBarChart = new Chart(ctx,
+        {
+            type: "bar",
+            data: data,
+            options: option_bars
+        });;
 });
 
 $(function () {
@@ -89,6 +101,7 @@ $(function () {
     Chart.defaults.global.responsive = true;
     ctx = $('#jumbotron-line-2-chart').get(0).getContext('2d');
     options = {
+        responsive: true,
         showScale: false,
         scaleShowGridLines: false,
         scaleGridLineColor: "rgba(0,0,0,.05)",
@@ -109,27 +122,34 @@ $(function () {
     data = {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
         datasets: [
-          {
-              label: "My First dataset",
-              fillColor: "rgba(26, 188, 156,0.2)",
-              strokeColor: "#1ABC9C",
-              pointColor: "#1ABC9C",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "#1ABC9C",
-              data: [65, 59, 80, 81, 56, 55, 40]
-          }, {
-              label: "My Second dataset",
-              fillColor: "rgba(34, 167, 240,0.2)",
-              strokeColor: "#22A7F0",
-              pointColor: "#22A7F0",
-              pointStrokeColor: "#fff",
-              pointHighlightFill: "#fff",
-              pointHighlightStroke: "#22A7F0",
-              data: [28, 48, 40, 19, 86, 27, 90]
-          }
+            {
+                label: "My First dataset",
+                fill: true,
+                backgroundColor: "rgba(26, 188, 156,0.2)",
+                strokeColor: "#1ABC9C",
+                pointBackgroundColor: "#1ABC9C",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "#1ABC9C",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            }, {
+                label: "My Second dataset",
+                fill: true,
+                backgroundColor: "rgba(34, 167, 240,0.2)",
+                strokeColor: "#22A7F0",
+                pointBackgroundColor: "#22A7F0",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "#22A7F0",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
         ]
     };
-    myLineChart = new Chart(ctx).Line(data, options);
+    myLineChart = new Chart(ctx,
+        {
+            type: "line",
+            data: data,
+            options: options
+        });
 });
 

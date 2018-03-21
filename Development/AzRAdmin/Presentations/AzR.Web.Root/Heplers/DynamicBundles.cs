7aeb2aac00c9d7716c5fd0c01083997f55f18290
ashiquzzaman <md.ashiquzzaman@datasoft-bd.com>
2018-03-21 10:43:21 +0600

    separate theme.

---
+++ Development/AzRAdmin/Presentations/AzR.Web.Root/Heplers/DynamicBundles.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AzR.Web.Root.Heplers
+{
+    public static class DynamicBundles
+    {
+        public static IHtmlString RenderSkin(string skin)
+        {
+            BundleTable.Bundles.Add(new StyleBundle("~/Content/admin-theme-" + skin).Include(
+                "~/Content/app/azr-" + skin + ".css",
+                "~/Content/app/style.css"));
+
+            return Styles.Render("~/Content/admin-theme-" + skin);
+        }
+    }
+}
